a2b2312fc48ec262570d8b6f2cc32d5af8aee2c0
davidlaprade <david.laprade@gmail.com>
2014-07-04 19:32:30 -0400

    Reservester commit 8

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,20 @@
 class RestaurantsController < ApplicationController
 	def new
+		@restaurant = Restaurant.new
+	end
+
+	def update
+  		@restaurant = Restaurant.find(params[:id])
+ 
+  		if @restaurant.update(article_params)
+    		redirect_to @restaurant
+ 	 	else
+    	render 'edit'
+  	end
+	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
 	end
 
 	def index
@@ -9,8 +24,11 @@ class RestaurantsController < ApplicationController
 	def create
 		@restaurant = Restaurant.new(restaurant_params)
 
-		@restaurant.save
-		redirect_to @restaurant
+		if @restaurant.save
+			redirect_to @restaurant
+		else
+			render 'new'
+		end
 	end
 
 	def show
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,7 +4,7 @@ class Restaurant < ActiveRecord::Base
 							length: {minimum: 5}
 	validates :address, presence:true
 	validates :phone, presence:true,
-					  format: { with: /\d\d\d-\d\d\d-\d\d\d\d/, message: "Must be in xxx-xxx-xxxx format"}
+					  format: { with: /\d\d\d-\d\d\d-\d\d\d\d/, message: "must be in xxx-xxx-xxxx format"}
 					  #makes sure the phone number is in xxx-xxx-xxxx format
 
 end
