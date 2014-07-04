e803b3bb42469a14614ac3ed66569abf4f29f971
davidlaprade <david.laprade@gmail.com>
2014-07-04 12:45:15 -0400

    Reservester commit 6

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ActiveRecord::Base
+	validates :name, presence: true
+	validates :description, presence:true,
+							length: {minimum: 5}
+	validates :address, presence:true
+	validates :phone, presence:true
+
 end
