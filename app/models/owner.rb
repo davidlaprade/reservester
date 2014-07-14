66ab328268b60b2b756b33303b856557466d6b3a
davidlaprade <david.laprade@gmail.com>
2014-07-14 19:48:06 -0400

    7-14-14 7:48pm

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,4 +3,7 @@ class Owner < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_uniqueness_of :email_address
+  has_many :restaurants
 end
--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-	validates_uniqueness_of :email_address
-	has_many :restaurants
-		
-	end
-end
