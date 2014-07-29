c8190fbad33ff381eb6bb0b33d4b4e3e4fcc14f4
davidlaprade <david.laprade@gmail.com>
2014-07-29 09:39:39 -0400

    adding star creation

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,2 +1,25 @@
 class StarsController < ApplicationController
+
+	def new
+		@star = current_user.stars.new
+		@star.restaurant_id = params[:star][:restaurant_id]
+		redirect_to restaurant_stars(@star.restaurant_id)
+	end
+
+	def create
+		@star = current_user.stars.new
+		@star.restaurant_id = params[:restaurant][:restaurant_id]
+		
+		if @star.save
+			redirect_to @restaurant
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def star_params
+		params.require(:star).permit(:restaurant_id)
+	end
+
 end
