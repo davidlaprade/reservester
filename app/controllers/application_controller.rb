60d35dab8b77dec4958e4d0ff9eceb4597e121fc
davidlaprade <david.laprade@gmail.com>
2014-09-20 13:12:24 -0400

    allowed users to edit their account details, including their role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,15 @@ class ApplicationController < ActionController::Base
 
   # to add new attribute to users model
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :email
+    devise_parameter_sanitizer.for(:sign_up) << :role
+    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:account_update) << :email
+    devise_parameter_sanitizer.for(:account_update) << :role
   end
 
 end
